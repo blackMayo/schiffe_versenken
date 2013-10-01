71f025dc750f2d0601c33af32afbd146c1c326a1
Ioanna Vletsou <ioanna.vletsou@mail.com>
2013-10-01 23:09:20 +0200

    Setting up a playground-frame putting an image into a panel.

---
+++ src/main/java/playgrid/BattleshipsPlaygroundGUI.java
@@ -0,0 +1,69 @@
+package main.java.playgrid;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * JavaDoc
+ * <p/>
+ * User: ioanna Date: 2013/10/01  21:12
+ */
+public class BattleshipsPlaygroundGUI {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+        createAndShowGUI();
+      }
+    });
+
+  }
+
+  private static void createAndShowGUI() {
+    JFrame.setDefaultLookAndFeelDecorated(true);
+
+    JFrame guiFrame = new JFrame("***BATTLESHIPS***");
+    guiFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+    BattleshipsPanel battleshipsPanel = new BattleshipsPanel();
+    battleshipsPanel.setBackground(Color.LIGHT_GRAY);
+    int width = battleshipsPanel.getImageWidth();
+    int height = battleshipsPanel.getImageHeight();
+
+    guiFrame.setContentPane(battleshipsPanel);
+    guiFrame.setSize(width, height);
+    guiFrame.setVisible(true);
+  }
+
+  private static class BattleshipsPanel extends JPanel {
+    private BufferedImage image;
+
+    public BattleshipsPanel() {
+      try {
+        InputStream input =
+                getClass().getResourceAsStream("/src/main/resources/images/schiffe_versenken-spielgrid.gif");
+        image = ImageIO.read(input);
+      } catch (IOException ex) {
+        ex.printStackTrace();
+      }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      g.drawImage(image, 0, 0, null); // see javadoc for more info on the parameters
+    }
+
+    private int getImageWidth() {
+      return image.getWidth();
+    }
+
+    private int getImageHeight() {
+      return image.getHeight();
+    }
+  }
+}
